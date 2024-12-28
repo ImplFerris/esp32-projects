021f7bcba05b37b0730e643b7722faf80ad8a32b
ImplFerris <implferris@proton.me>
2024-12-28 18:07:12 +0530

    ultrasonic

---
+++ ultrasonic/src/bin/main.rs
@@ -0,0 +1,90 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Input, Level, Output, Pull},
+    ledc::{
+        channel::{self, ChannelIFace},
+        timer::{self, TimerIFace},
+        HighSpeed, Ledc,
+    },
+    prelude::*,
+    rtc_cntl::Rtc,
+};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+    // let led = peripherals.GPIO2; // uses onboard LED
+    let led = peripherals.GPIO33;
+    let ledc = Ledc::new(peripherals.LEDC);
+    let mut hstimer0 = ledc.timer::<HighSpeed>(timer::Number::Timer0);
+    hstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::HSClockSource::APBClk,
+            frequency: 24.kHz(),
+        })
+        .unwrap();
+
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: &hstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
+
+    // For HC-SR04 Ultrasonic
+    let mut trig = Output::new(peripherals.GPIO5, Level::Low);
+    let echo = Input::new(peripherals.GPIO18, Pull::Down);
+
+    let delay = Delay::new();
+    let rtc = Rtc::new(peripherals.LPWR);
+
+    loop {
+        delay.delay_millis(50);
+        trig.set_low();
+        delay.delay_micros(2);
+        trig.set_high();
+        delay.delay_micros(10);
+        trig.set_low();
+
+        while echo.is_low() {}
+        let time1 = rtc.current_time();
+
+        while echo.is_high() {}
+        let time2 = rtc.current_time();
+
+        let pulse_width = match (time2 - time1).num_microseconds() {
+            Some(pw) => pw as f64,
+            None => continue,
+        };
+
+        let distance = (pulse_width * 0.0343) / 2.0;
+
+        // esp_println::println!("Pulse Width: {}", pulse_width);
+        // esp_println::println!("Distance: {}", distance);
+
+        let duty_pct: u8 = if distance < 30.0 {
+            let ratio = (30.0 - distance) / 30.0;
+            let p = (ratio * 100.0) as u8;
+            p.min(100)
+        } else {
+            0
+        };
+
+        if let Err(e) = channel0.set_duty(duty_pct) {
+            esp_println::println!("Failed to set duty cycle: {:?}", e);
+        }
+        delay.delay_millis(60);
+    }
+}
