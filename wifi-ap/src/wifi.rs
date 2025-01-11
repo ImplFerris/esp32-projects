3ff1abaa1246ba049822e4de7d884c65cb96ea62
ImplFerris <implferris@proton.me>
2025-01-11 14:44:38 +0530

    access point

---
+++ wifi-ap/src/wifi.rs
@@ -0,0 +1,121 @@
+use core::{net::Ipv4Addr, str::FromStr};
+
+use anyhow::anyhow;
+use embassy_executor::Spawner;
+use embassy_net::{Ipv4Cidr, Runner, Stack, StackResources, StaticConfigV4};
+use embassy_time::{Duration, Timer};
+use esp_alloc as _;
+use esp_backtrace as _;
+use esp_hal::rng::Rng;
+use esp_println::println;
+use esp_wifi::{
+    wifi::{
+        AccessPointConfiguration, Configuration, WifiApDevice, WifiController, WifiDevice,
+        WifiEvent, WifiState,
+    },
+    EspWifiController,
+};
+
+use crate::mk_static;
+extern crate alloc;
+
+// Unlike Station mode, You can give any IP range(private) that you like
+// IP Address/Subnet mask eg: STATIC_IP=192.168.13.37/24
+const STATIC_IP: &str = "192.168.13.37/24";
+// Gateway IP eg: GATEWAY_IP="192.168.13.1"
+const GATEWAY_IP: &str = "192.168.13.1";
+
+#[embassy_executor::task]
+async fn connection_task(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        if esp_wifi::wifi::wifi_state() == WifiState::ApStarted {
+            // wait until we're no longer connected
+            controller.wait_for_event(WifiEvent::ApStop).await;
+            Timer::after(Duration::from_millis(5000)).await
+        }
+
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::AccessPoint(AccessPointConfiguration {
+                ssid: "implRust".try_into().unwrap(), // Use whatever Wi-Fi name you want
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start_async().await.unwrap();
+            println!("Wifi started!");
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn net_task(runner: &'static mut Runner<'static, WifiDevice<'static, WifiApDevice>>) -> ! {
+    runner.run().await
+}
+
+pub async fn start_wifi(
+    wifi_init: &'static EspWifiController<'static>,
+    wifi: esp_hal::peripherals::WIFI,
+    mut rng: Rng,
+    spawner: &Spawner,
+) -> anyhow::Result<&'static Stack<'static>> {
+    let (wifi_interface, controller) =
+        esp_wifi::wifi::new_with_mode(wifi_init, wifi, WifiApDevice).unwrap();
+    let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
+
+    // Parse STATIC_IP
+    let ip_addr =
+        Ipv4Cidr::from_str(STATIC_IP).map_err(|_| anyhow!("Invalid STATIC_IP: {}", STATIC_IP))?;
+
+    // Parse GATEWAY_IP
+    let gateway = Ipv4Addr::from_str(GATEWAY_IP)
+        .map_err(|_| anyhow!("Invalid GATEWAY_IP: {}", GATEWAY_IP))?;
+
+    let net_config = embassy_net::Config::ipv4_static(StaticConfigV4 {
+        address: ip_addr,
+        gateway: Some(gateway),
+        dns_servers: Default::default(),
+    });
+
+    // alternate approach
+    // let net_config = embassy_net::Config::ipv4_static(StaticConfigV4 {
+    //     address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 2, 1), 24),
+    //     gateway: Some(Ipv4Address::from_bytes(&[192, 168, 2, 1])),
+    //     dns_servers: Default::default(),
+    // });
+
+    let (stack, runner) = mk_static!(
+        (
+            Stack<'static>,
+            Runner<'static, WifiDevice<'static, WifiApDevice>>
+        ),
+        embassy_net::new(
+            wifi_interface,
+            net_config,
+            mk_static!(StackResources<3>, StackResources::<3>::new()),
+            net_seed
+        )
+    );
+
+    spawner.spawn(connection_task(controller)).ok();
+    spawner.spawn(net_task(runner)).ok();
+
+    loop {
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    println!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config_v4() {
+            println!("Got IP: {}", config.address);
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    Ok(stack)
+}
