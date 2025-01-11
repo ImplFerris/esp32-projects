842d394e4e9d031bcf69e4bdae523563269cdaa8
ImplFerris <implferris@proton.me>
2025-01-11 15:48:27 +0530

    module

--- webserver-html/src/bin/async_main.rs
+++ webserver-html/src/bin/async_main.rs
@@ -1,51 +1,17 @@
 #![no_std]
 #![no_main]
-#![feature(impl_trait_in_assoc_type)]
 
 use embassy_executor::Spawner;
-use embassy_net::{DhcpConfig, Runner, Stack, StackResources};
-use embassy_time::{Duration, Timer};
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{prelude::*, rng::Rng};
 use esp_println::println;
-use esp_wifi::{
-    wifi::{
-        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-        WifiState,
-    },
-    EspWifiController,
-};
+use esp_wifi::EspWifiController;
 use log::info;
-use picoserve::{response::File, routing, AppBuilder, AppRouter};
 
 extern crate alloc;
 
-macro_rules! mk_static {
-    ($t:ty,$val:expr) => {{
-        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
-        #[deny(unused_attributes)]
-        let x = STATIC_CELL.uninit().write(($val));
-        x
-    }};
-}
-
-struct Application;
-
-impl AppBuilder for Application {
-    type PathRouter = impl routing::PathRouter;
-
-    fn build_app(self) -> picoserve::Router<Self::PathRouter> {
-        picoserve::Router::new().route(
-            "/",
-            routing::get_service(File::html(include_str!("index.html"))),
-        )
-    }
-}
-
-const WEB_TASK_POOL_SIZE: usize = 2;
-const SSID: &str = env!("SSID");
-const PASSWORD: &str = env!("PASSWORD");
+use webserver as lib;
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -62,139 +28,22 @@ async fn main(spawner: Spawner) {
     let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
-    let mut rng = Rng::new(peripherals.RNG);
-    let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
+    let rng = Rng::new(peripherals.RNG);
 
     info!("Embassy initialized!");
 
     let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
 
-    let wifi_init = &*mk_static!(
+    let wifi_init = lib::mk_static!(
         EspWifiController<'static>,
-        esp_wifi::init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
+        esp_wifi::init(timg0.timer0, rng, peripherals.RADIO_CLK).unwrap()
     );
 
-    let wifi = peripherals.WIFI;
+    let stack = lib::wifi::start_wifi(wifi_init, peripherals.WIFI, rng, &spawner).await;
 
-    let (wifi_interface, controller) =
-        esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiStaDevice).unwrap();
-
-    let dhcp_config = DhcpConfig::default();
-
-    let net_config = embassy_net::Config::dhcpv4(dhcp_config);
-
-    let (stack, runner) = mk_static!(
-        (
-            Stack<'static>,
-            Runner<'static, WifiDevice<'static, WifiStaDevice>>
-        ),
-        embassy_net::new(
-            wifi_interface,
-            net_config,
-            mk_static!(StackResources<3>, StackResources::<3>::new()),
-            net_seed
-        )
-    );
-
-    spawner.spawn(connection_task(controller)).ok();
-    spawner.spawn(net_task(runner)).ok();
-
-    loop {
-        if stack.is_link_up() {
-            break;
-        }
-        Timer::after(Duration::from_millis(500)).await;
-    }
-
-    println!("Waiting to get IP address...");
-    loop {
-        if let Some(config) = stack.config_v4() {
-            println!("Got IP: {}", config.address);
-            break;
-        }
-        Timer::after(Duration::from_millis(500)).await;
-    }
-
-    let app = picoserve::make_static!(AppRouter<Application>, Application.build_app());
-
-    let config = picoserve::make_static!(
-        picoserve::Config<Duration>,
-        picoserve::Config::new(picoserve::Timeouts {
-            start_read_request: Some(Duration::from_secs(5)),
-            read_request: Some(Duration::from_secs(1)),
-            write: Some(Duration::from_secs(1)),
-        })
-        .keep_connection_alive()
-    );
-
-    for id in 0..WEB_TASK_POOL_SIZE {
-        spawner.must_spawn(web_task(id, *stack, app, config));
+    let web = lib::web::WebApp::default();
+    for id in 0..lib::web::WEB_TASK_POOL_SIZE {
+        spawner.must_spawn(lib::web::web_task(id, *stack, web.app, web.config));
     }
     println!("Web server started...");
-}
-
-#[embassy_executor::task]
-async fn connection_task(mut controller: WifiController<'static>) {
-    println!("start connection task");
-    println!("Device capabilities: {:?}", controller.capabilities());
-    loop {
-        match esp_wifi::wifi::wifi_state() {
-            WifiState::StaConnected => {
-                // wait until we're no longer connected
-                controller.wait_for_event(WifiEvent::StaDisconnected).await;
-                Timer::after(Duration::from_millis(5000)).await
-            }
-            _ => {}
-        }
-        if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::Client(ClientConfiguration {
-                ssid: SSID.try_into().unwrap(),
-                password: PASSWORD.try_into().unwrap(),
-                ..Default::default()
-            });
-            controller.set_configuration(&client_config).unwrap();
-            println!("Starting wifi");
-            controller.start_async().await.unwrap();
-            println!("Wifi started!");
-        }
-        println!("About to connect...");
-
-        match controller.connect_async().await {
-            Ok(_) => println!("Wifi connected!"),
-            Err(e) => {
-                println!("Failed to connect to wifi: {e:?}");
-                Timer::after(Duration::from_millis(5000)).await
-            }
-        }
-    }
-}
-
-#[embassy_executor::task]
-async fn net_task(runner: &'static mut Runner<'static, WifiDevice<'static, WifiStaDevice>>) -> ! {
-    runner.run().await
-}
-
-#[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
-async fn web_task(
-    id: usize,
-    stack: Stack<'static>,
-    app: &'static AppRouter<Application>,
-    config: &'static picoserve::Config<Duration>,
-) -> ! {
-    let port = 80;
-    let mut tcp_rx_buffer = [0; 1024];
-    let mut tcp_tx_buffer = [0; 1024];
-    let mut http_buffer = [0; 2048];
-
-    picoserve::listen_and_serve(
-        id,
-        app,
-        config,
-        stack,
-        port,
-        &mut tcp_rx_buffer,
-        &mut tcp_tx_buffer,
-        &mut http_buffer,
-    )
-    .await
 }
