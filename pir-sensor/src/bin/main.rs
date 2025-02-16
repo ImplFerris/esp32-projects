e11e21ce7aac0e090df7b53884f2ce9e31d305b3
ImplFerris <implferris@proton.me>
2025-02-16 22:20:50 +0530

    pir sensor

---
+++ pir-sensor/src/bin/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Input, Pull};
+use esp_hal::{peripheral, prelude::*};
+use esp_println::println;
+use log::info;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+    esp_println::logger::init_logger_from_env();
+
+    let sensor_pin = Input::new(peripherals.GPIO33, Pull::Down);
+
+    let delay = Delay::new();
+    loop {
+        if sensor_pin.is_high() {
+            println!("Motion detected");
+        } else {
+            println!("Motion ended");
+        }
+        delay.delay(500.millis());
+    }
+}
