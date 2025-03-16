102cbea1d0dbe8f1b1874be0682fd885bd6483e7
ImplFerris <implferris@proton.me>
2025-03-16 11:58:27 +0530

    wifi led migrated to esp-hal 1.0.0-beta

--- webserver-base/src/wifi.rs
+++ webserver-base/src/wifi.rs
@@ -1,30 +1,23 @@
-use core::{net::Ipv4Addr, str::FromStr};
+use core::net::Ipv4Addr;
+use core::str::FromStr;
 
 use embassy_executor::Spawner;
 use embassy_net::{Ipv4Cidr, Runner, Stack, StackResources, StaticConfigV4};
 use embassy_time::{Duration, Timer};
-use esp_alloc as _;
-use esp_backtrace as _;
 use esp_hal::rng::Rng;
+use esp_println as _;
 use esp_println::println;
-use esp_wifi::{
-    wifi::{
-        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-        WifiState,
-    },
-    EspWifiController,
-};
+use esp_wifi::wifi::{self, WifiController, WifiDevice, WifiEvent, WifiState};
+use esp_wifi::EspWifiController;
 use heapless::Vec;
 
 use crate::mk_static;
-extern crate alloc;
 
 const SSID: &str = env!("SSID");
 const PASSWORD: &str = env!("PASSWORD");
 
 // IP Address/Subnet mask eg: STATIC_IP=192.168.0.50/24
 const STATIC_IP: &str = env!("STATIC_IP");
-// Gateway IP eg: GATEWAY_IP=192.168.0.1
 const GATEWAY_IP: &str = env!("GATEWAY_IP");
 
 #[embassy_executor::task]
@@ -41,7 +34,7 @@ async fn connection_task(mut controller: WifiController<'static>) {
             _ => {}
         }
         if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::Client(ClientConfiguration {
+            let client_config = wifi::Configuration::Client(wifi::ClientConfiguration {
                 ssid: SSID.try_into().unwrap(),
                 password: PASSWORD.try_into().unwrap(),
                 ..Default::default()
@@ -56,7 +49,7 @@ async fn connection_task(mut controller: WifiController<'static>) {
         match controller.connect_async().await {
             Ok(_) => println!("Wifi connected!"),
             Err(e) => {
-                println!("Failed to connect to wifi: {e:?}");
+                println!("Failed to connect to wifi: {:?}", e);
                 Timer::after(Duration::from_millis(5000)).await
             }
         }
@@ -64,20 +57,21 @@ async fn connection_task(mut controller: WifiController<'static>) {
 }
 
 #[embassy_executor::task]
-async fn net_task(runner: &'static mut Runner<'static, WifiDevice<'static, WifiStaDevice>>) -> ! {
+async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
     runner.run().await
 }
 
 pub async fn start_wifi(
-    wifi_init: &'static EspWifiController<'static>,
+    esp_wifi_ctrl: &'static EspWifiController<'static>,
     wifi: esp_hal::peripherals::WIFI,
     mut rng: Rng,
     spawner: &Spawner,
-) -> &'static Stack<'static> {
-    let (wifi_interface, controller) =
-        esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiStaDevice).unwrap();
+) -> Stack<'static> {
+    let (controller, interfaces) = esp_wifi::wifi::new(&esp_wifi_ctrl, wifi).unwrap();
+    let wifi_interface = interfaces.sta;
     let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
 
+    //find the `let net_config` part and replace
     let Ok(ip_addr) = Ipv4Cidr::from_str(STATIC_IP) else {
         println!("Invalid STATIC_IP");
         loop {}
@@ -94,22 +88,24 @@ pub async fn start_wifi(
         dns_servers: Vec::new(),
     });
 
-    let (stack, runner) = mk_static!(
-        (
-            Stack<'static>,
-            Runner<'static, WifiDevice<'static, WifiStaDevice>>
-        ),
-        embassy_net::new(
-            wifi_interface,
-            net_config,
-            mk_static!(StackResources<3>, StackResources::<3>::new()),
-            net_seed
-        )
+    // Init network stack
+    let (stack, runner) = embassy_net::new(
+        wifi_interface,
+        net_config,
+        mk_static!(StackResources<3>, StackResources::<3>::new()),
+        net_seed,
     );
 
     spawner.spawn(connection_task(controller)).ok();
     spawner.spawn(net_task(runner)).ok();
 
+    wait_for_connection(stack).await;
+
+    stack
+}
+
+async fn wait_for_connection(stack: Stack<'_>) {
+    println!("Waiting for link to be up");
     loop {
         if stack.is_link_up() {
             break;
@@ -125,6 +121,4 @@ pub async fn start_wifi(
         }
         Timer::after(Duration::from_millis(500)).await;
     }
-
-    stack
 }
--- wifi-led/src/web.rs
+++ wifi-led/src/web.rs
@@ -6,56 +6,6 @@ use picoserve::{
     routing, AppBuilder, AppRouter, Router,
 };
 
-pub const WEB_TASK_POOL_SIZE: usize = 2;
-
-#[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
-pub async fn web_task(
-    id: usize,
-    stack: Stack<'static>,
-    app: &'static AppRouter<Application>,
-    config: &'static picoserve::Config<Duration>,
-) -> ! {
-    let port = 80;
-    let mut tcp_rx_buffer = [0; 1024];
-    let mut tcp_tx_buffer = [0; 1024];
-    let mut http_buffer = [0; 2048];
-
-    picoserve::listen_and_serve(
-        id,
-        app,
-        config,
-        stack,
-        port,
-        &mut tcp_rx_buffer,
-        &mut tcp_tx_buffer,
-        &mut http_buffer,
-    )
-    .await
-}
-
-pub struct WebApp {
-    pub app: &'static Router<<Application as AppBuilder>::PathRouter>,
-    pub config: &'static picoserve::Config<Duration>,
-}
-
-impl Default for WebApp {
-    fn default() -> Self {
-        let app = picoserve::make_static!(AppRouter<Application>, Application.build_app());
-
-        let config = picoserve::make_static!(
-            picoserve::Config<Duration>,
-            picoserve::Config::new(picoserve::Timeouts {
-                start_read_request: Some(Duration::from_secs(5)),
-                read_request: Some(Duration::from_secs(1)),
-                write: Some(Duration::from_secs(1)),
-            })
-            .keep_connection_alive()
-        );
-
-        Self { app, config }
-    }
-}
-
 pub struct Application;
 
 impl AppBuilder for Application {
@@ -68,6 +18,56 @@ impl AppBuilder for Application {
                 routing::get_service(File::html(include_str!("index.html"))),
             )
             .route("/led", routing::post(led_handler))
+    }
+}
+
+pub const WEB_TASK_POOL_SIZE: usize = 2;
+
+#[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
+pub async fn web_task(
+    id: usize,
+    stack: Stack<'static>,
+    router: &'static AppRouter<Application>,
+    config: &'static picoserve::Config<Duration>,
+) -> ! {
+    let port = 80;
+    let mut tcp_rx_buffer = [0; 1024];
+    let mut tcp_tx_buffer = [0; 1024];
+    let mut http_buffer = [0; 2048];
+
+    picoserve::listen_and_serve(
+        id,
+        router,
+        config,
+        stack,
+        port,
+        &mut tcp_rx_buffer,
+        &mut tcp_tx_buffer,
+        &mut http_buffer,
+    )
+    .await
+}
+
+pub struct WebApp {
+    pub router: &'static Router<<Application as AppBuilder>::PathRouter>,
+    pub config: &'static picoserve::Config<Duration>,
+}
+
+impl Default for WebApp {
+    fn default() -> Self {
+        let router = picoserve::make_static!(AppRouter<Application>, Application.build_app());
+
+        let config = picoserve::make_static!(
+            picoserve::Config<Duration>,
+            picoserve::Config::new(picoserve::Timeouts {
+                start_read_request: Some(Duration::from_secs(5)),
+                read_request: Some(Duration::from_secs(1)),
+                write: Some(Duration::from_secs(1)),
+            })
+            .keep_connection_alive()
+        );
+
+        Self { router, config }
     }
 }
 
