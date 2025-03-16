f439f0110d6224701b494ccc4822eac18ecf1a53
ImplFerris <implferris@proton.me>
2025-03-16 15:50:15 +0530

    oled module migrated to esp-hal 1.0.0-beta

--- oled-bmp/src/bin/main.rs
+++ oled-bmp/src/bin/main.rs
@@ -1,47 +1,49 @@
 #![no_std]
 #![no_main]
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use embedded_graphics::{image::Image, prelude::Point};
-use esp_backtrace as _;
-use esp_hal::prelude::*;
-use log::info;
-use ssd1306::{
-    mode::DisplayConfigAsync, prelude::DisplayRotation, size::DisplaySize128x64,
-    I2CDisplayInterface, Ssd1306Async,
-};
-
+use embedded_graphics::image::Image;
+use embedded_graphics::prelude::Point;
 use embedded_graphics::prelude::*;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{clock::CpuClock, time::Rate};
+use esp_println as _;
+use ssd1306::mode::DisplayConfigAsync;
+use ssd1306::{
+    prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306Async,
+};
 use tinybmp::Bmp;
 
-#[main]
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
-    let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    // generator version: 0.3.1
 
-    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
 
-    let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
     info!("Embassy initialized!");
 
-    let i2c0 = esp_hal::i2c::master::I2c::new(
+    let i2c_bus = esp_hal::i2c::master::I2c::new(
         peripherals.I2C0,
-        esp_hal::i2c::master::Config {
-            frequency: 400.kHz(),
-            timeout: Some(100),
-        },
+        esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
     )
+    .unwrap()
     .with_scl(peripherals.GPIO18)
     .with_sda(peripherals.GPIO23)
     .into_async();
 
-    let interface = I2CDisplayInterface::new(i2c0);
+    let interface = I2CDisplayInterface::new(i2c_bus);
+
     // initialize the display
     let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
@@ -53,6 +55,7 @@ async fn main(_spawner: Spawner) {
     // Parse the BMP file.
     let bmp = Bmp::from_slice(bmp_data).unwrap();
 
+    // usual code:
     let image = Image::new(&bmp, Point::new(32, 0));
     image.draw(&mut display).unwrap();
     display.flush().await.unwrap();
--- oled-image/src/bin/main.rs
+++ oled-image/src/bin/main.rs
@@ -1,22 +1,25 @@
 #![no_std]
 #![no_main]
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use embedded_graphics::{
-    image::{Image, ImageRaw},
-    pixelcolor::BinaryColor,
-    prelude::Point,
-};
-use esp_backtrace as _;
-use esp_hal::prelude::*;
-use log::info;
+use embedded_graphics::image::{Image, ImageRaw};
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::Point;
+use embedded_graphics::prelude::*;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{clock::CpuClock, time::Rate};
+use esp_println as _;
+use ssd1306::mode::DisplayConfigAsync;
 use ssd1306::{
-    mode::DisplayConfigAsync, prelude::DisplayRotation, size::DisplaySize128x64,
-    I2CDisplayInterface, Ssd1306Async,
+    prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306Async,
 };
 
-use embedded_graphics::prelude::*;
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
 
 #[rustfmt::skip]
 const IMG_DATA: &[u8] = &[
@@ -27,33 +30,29 @@ const IMG_DATA: &[u8] = &[
     0b11101110,
 ];
 
-#[main]
+#[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
-    let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    // generator version: 0.3.1
 
-    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
 
-    let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
     info!("Embassy initialized!");
 
-    let i2c0 = esp_hal::i2c::master::I2c::new(
+    let i2c_bus = esp_hal::i2c::master::I2c::new(
         peripherals.I2C0,
-        esp_hal::i2c::master::Config {
-            frequency: 400.kHz(),
-            timeout: Some(100),
-        },
+        esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
     )
+    .unwrap()
     .with_scl(peripherals.GPIO18)
     .with_sda(peripherals.GPIO23)
     .into_async();
 
-    let interface = I2CDisplayInterface::new(i2c0);
+    let interface = I2CDisplayInterface::new(i2c_bus);
+
     // initialize the display
     let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
