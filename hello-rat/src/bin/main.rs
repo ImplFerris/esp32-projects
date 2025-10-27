a7f7f1a9f2fbc93fa48221e34cd5030b25968cf2
ImplFerris <implferris@proton.me>
2025-10-27 23:24:05 +0530

    Ratatui and mousefood in no_std embedded systems

---
+++ hello-rat/src/bin/main.rs
@@ -0,0 +1,119 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use defmt::info;
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use esp_println as _;
+
+// Embedded Grpahics related
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+
+// ESP32 SPI + Display Driver bridge
+use display_interface_spi::SPIInterface;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_hal::delay::Delay;
+use esp_hal::spi::Mode as SpiMode;
+use esp_hal::spi::master::Config as SpiConfig;
+use esp_hal::spi::master::Spi;
+use esp_hal::time::Rate; // For specifying SPI frequency
+use ili9341::{DisplaySize240x320, Ili9341, Orientation};
+
+// For managing GPIO state
+use esp_hal::gpio::{Level, Output, OutputConfig};
+
+// For ratatui
+use mousefood::{EmbeddedBackend, EmbeddedBackendConfig};
+use ratatui::layout::{Constraint, Flex, Layout};
+use ratatui::widgets::{Block, Paragraph, Wrap};
+use ratatui::{Frame, Terminal, style::*};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    // generator version: 0.6.0
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 98767);
+
+    // Initialize SPI
+    let spi = Spi::new(
+        peripherals.SPI2,
+        SpiConfig::default()
+            .with_frequency(Rate::from_mhz(60))
+            .with_mode(SpiMode::_0),
+    )
+    .unwrap()
+    //CLK
+    .with_sck(peripherals.GPIO18)
+    //DIN
+    .with_mosi(peripherals.GPIO23);
+    let cs = Output::new(peripherals.GPIO15, Level::Low, OutputConfig::default());
+    let dc = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+    let reset = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
+
+    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+    let interface = SPIInterface::new(spi_dev, dc);
+
+    let mut display = Ili9341::new(
+        interface,
+        reset,
+        &mut Delay::new(),
+        Orientation::Landscape,
+        DisplaySize240x320,
+    )
+    .unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
+
+    let backend = EmbeddedBackend::new(&mut display, EmbeddedBackendConfig::default());
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    loop {
+        terminal.draw(draw).unwrap();
+    }
+}
+
+fn draw(frame: &mut Frame) {
+    let outer_block = Block::bordered()
+        .border_style(Style::new().green())
+        .title(" ESP32 Dashboard ");
+    frame.render_widget(outer_block, frame.area());
+
+    let vertical_layout = Layout::vertical([Constraint::Length(3)])
+        .flex(Flex::Center)
+        .split(frame.area());
+
+    let horizontal_layout = Layout::horizontal([Constraint::Length(25)])
+        .flex(Flex::Center)
+        .split(vertical_layout[0]);
+
+    let text = "Rat(a tui) inside ESP32";
+    let paragraph = Paragraph::new(text.dark_gray())
+        .wrap(Wrap { trim: true })
+        .centered();
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().yellow())
+        .title(" impl Rust ");
+
+    frame.render_widget(paragraph.block(bordered_block), horizontal_layout[0]);
+}
