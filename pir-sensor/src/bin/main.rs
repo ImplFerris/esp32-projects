d5b9fecc0eaeed83bf8d724654fd2e80787825c2
ImplFerris <implferris@proton.me>
2025-11-01 23:41:12 +0530

    migrated pir sensor to esp-hal 1.0.0

--- pir-sensor/src/bin/main.rs
+++ pir-sensor/src/bin/main.rs
@@ -1,5 +1,10 @@
 #![no_std]
 #![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
 use defmt::info;
 use esp_hal::clock::CpuClock;
@@ -13,9 +18,13 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
 #[main]
 fn main() -> ! {
-    // generator version: 0.3.1
+    // generator version: 1.0.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
