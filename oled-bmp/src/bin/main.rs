5a5dbfa29af3cb81a29e07380b925812355c1206
ImplFerris <implferris@proton.me>
2025-11-08 08:09:06 +0530

    oled migrated to esp-hal 1.0.0

--- oled-bmp/src/bin/main.rs
+++ oled-bmp/src/bin/main.rs
@@ -1,41 +1,57 @@
 #![no_std]
 #![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use embedded_graphics::image::Image;
-use embedded_graphics::prelude::Point;
-use embedded_graphics::prelude::*;
+use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
-use esp_hal::{clock::CpuClock, time::Rate};
 use esp_println as _;
-use ssd1306::mode::DisplayConfigAsync;
-use ssd1306::{
-    prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306Async,
-};
-use tinybmp::Bmp;
+
+// I2C
+use esp_hal::i2c::master::Config as I2cConfig; // for convenience, importing as alias
+use esp_hal::i2c::master::I2c;
+use esp_hal::time::Rate;
+
+// OLED
+use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::*};
+
+// Embedded Graphics
+use embedded_graphics::{image::Image, prelude::Point, prelude::*};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[esp_hal_embassy::main]
-async fn main(_spawner: Spawner) {
-    // generator version: 0.3.1
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.0.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    let timer0 = TimerGroup::new(peripherals.TIMG1);
-    esp_hal_embassy::init(timer0.timer0);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
 
     info!("Embassy initialized!");
 
-    let i2c_bus = esp_hal::i2c::master::I2c::new(
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    let i2c_bus = I2c::new(
         peripherals.I2C0,
-        esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
+        // I2cConfig is alias of esp_hal::i2c::master::I2c::Config
+        I2cConfig::default().with_frequency(Rate::from_khz(400)),
     )
     .unwrap()
     .with_scl(peripherals.GPIO18)
@@ -53,7 +69,7 @@ async fn main(_spawner: Spawner) {
     let bmp_data = include_bytes!("../ferris.bmp");
 
     // Parse the BMP file.
-    let bmp = Bmp::from_slice(bmp_data).unwrap();
+    let bmp = tinybmp::Bmp::from_slice(bmp_data).unwrap();
 
     // usual code:
     let image = Image::new(&bmp, Point::new(32, 0));
