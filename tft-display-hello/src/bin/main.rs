533426c69c030ffa86367cb278b634d406fc8f1a
ImplFerris <implferris@proton.me>
2025-11-12 17:54:12 +0530

    migrated tft display to esp-hal 1.0.0

--- tft-display-hello/src/bin/main.rs
+++ tft-display-hello/src/bin/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
-// Usual imports
 use defmt::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
@@ -21,9 +25,9 @@ use profont::{PROFONT_18_POINT, PROFONT_24_POINT};
 use display_interface_spi::SPIInterface;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_hal::delay::Delay;
+use esp_hal::spi::Mode as SpiMode;
 use esp_hal::spi::master::Config as SpiConfig;
 use esp_hal::spi::master::Spi;
-use esp_hal::spi::Mode as SpiMode;
 use esp_hal::time::Rate; // For specifying SPI frequency
 use ili9341::{DisplaySize240x320, Ili9341, Orientation};
 
@@ -35,8 +39,14 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
 #[main]
 fn main() -> ! {
+    // generator version: 1.0.0
+
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -56,7 +66,7 @@ fn main() -> ! {
     let dc = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
     let reset = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
 
-    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs);
+    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
     let interface = SPIInterface::new(spi_dev, dc);
 
     let mut display = Ili9341::new(
@@ -71,7 +81,6 @@ fn main() -> ! {
     display.clear(Rgb565::WHITE).unwrap();
 
     let text_style = MonoTextStyle::new(&PROFONT_24_POINT, Rgb565::RED);
-
     Text::with_baseline("impl Rust", Point::new(50, 150), text_style, Baseline::Top)
         .draw(&mut display)
         .unwrap();
@@ -85,6 +94,8 @@ fn main() -> ! {
     loop {
         info!("Hello world!");
         let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(5000) {}
+        while delay_start.elapsed() < Duration::from_millis(500) {}
     }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
